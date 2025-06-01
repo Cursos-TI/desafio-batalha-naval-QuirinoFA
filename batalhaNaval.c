7823d9b57d9f9134d6e2ea24e70fb4f748d9973b
Felipe Araújo Quirino <felipeaquirino@gmail.com>
2025-06-01 12:45:28 -0300

    Update batalhaNaval.c
    Implementação de ataques especiaisnas formas de:
    - cone;
    - cruz;
    - losango;

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -39,8 +39,16 @@ void navio_horizontal(int tabuleiro[10][10], int navio_h, int linha, int coluna)
         {
             if(i == linha && tamanho < navio_h && j >= coluna)
             {
-                tabuleiro[i][j] = navio_h;
-                tamanho++;
+                if (tabuleiro[i][j] == 0)
+                {
+                    tabuleiro[i][j] = navio_h;
+                    tamanho++;
+                }
+                else
+                {
+                    printf("ERRO! Navio sobreposto!\n");
+                    return;
+                }
             }
         }
     }
@@ -56,10 +64,111 @@ void navio_vertical(int tabuleiro[10][10], int navio_v, int linha, int coluna)
         {
             if(i >= linha && tamanho < navio_v && j == coluna)
             {
-                tabuleiro[i][j] = navio_v;
-                tamanho++;
+                if( tabuleiro[i][j] == 0)
+                {
+                    tabuleiro[i][j] = navio_v;
+                    tamanho++;
+                }
+                else
+                {
+                    printf("ERRO! Navio sobreposto!\n");
+                }
             }
         }
+    }
+    return;
+}
+
+void navio_diagonal_principal(int tabuleiro[10][10], int navio_v, int linha, int coluna)
+{
+    int tamanho = 0;
+    for(int i = 0; i < 10; i++)
+    {
+        for(int j = 0; j < 10; j++)
+        {
+            if(i == linha + tamanho && tamanho < navio_v && j == coluna + tamanho)
+            {
+                if( tabuleiro[i][j] == 0)
+                {
+                    tabuleiro[i][j] = navio_v;
+                    tamanho++;
+                }
+                else
+                {
+                    printf("ERRO! Navio sobreposto!\n");
+                }
+            }
+        }
+    }
+    return;
+}
+
+void navio_diagonal_secundaria(int tabuleiro[10][10], int navio_v, int linha, int coluna)
+{
+    int tamanho = 0;
+    for(int i = 0; i < 10; i++)
+    {
+        for(int j = 0; j < 10; j++)
+        {
+            if(i == linha + tamanho && tamanho < navio_v && j == coluna - tamanho)
+            {
+                if( tabuleiro[i][j] == 0)
+                {
+                    tabuleiro[i][j] = navio_v;
+                    tamanho++;
+                }
+                else
+                {
+                    printf("ERRO! Navio sobreposto!\n");
+                }
+            }
+        }
+    }
+    return;
+}
+
+void cone(int tabuleiro[10][10], int cone_linha, int cone_coluna)
+{
+    int count_cone = 0;
+    for(int i = cone_linha; i < cone_linha + 3; i++)
+    {
+        for(int j = cone_coluna - 2; j < cone_coluna + 3; j++)
+        {
+            if(j >= cone_coluna - count_cone && j <= cone_coluna + count_cone)
+                tabuleiro[i][j] = 1;
+        }
+        count_cone++;
+    }
+    return;
+}
+
+void cruz(int tabuleiro[10][10], int cruz_linha, int cruz_coluna)
+{
+    int count_cone = 0;
+    for(int i = cruz_linha - 2; i <  cruz_linha + 3; i++)
+    {
+        for(int j = cruz_coluna - 2; j < cruz_coluna + 3; j++)
+        {
+            if(i == cruz_linha || j == cruz_coluna)
+                tabuleiro[i][j] = 1;
+        }
+        count_cone++;
+    }
+    return;
+}
+
+void losango(int tabuleiro[10][10], int losango_linha, int losango_coluna)
+{
+    cone(tabuleiro, losango_linha - 2, losango_coluna);
+    int count_losango = 1;
+    for(int i = losango_linha + 1; i < losango_linha + 3; i++)
+    {
+        for(int j = losango_coluna - 2; j < losango_coluna + 3; j++)
+        {
+            if(j >= losango_coluna - count_losango && j <= losango_coluna + count_losango)
+                tabuleiro[i][j] = 1;
+        }
+        count_losango--;
     }
     return;
 }
@@ -68,26 +177,58 @@ int main()
 {
     // Iniciando as variáveis
     int tabuleiro[10][10] = {0};
+    int tam_navio = 3;
     
     // Mostrando tabuleiro para o usuário
     print_tabuleiro(tabuleiro);
     
     // Posição do navio horizontal
-    int horizontal_linha = 3;
-    int horizontal_coluna = 3;
+    int horizontal_linha = 1;
+    int horizontal_coluna = 1;
 
     // Posição do navio vertical
     int vertical_linha = 7;
     int vertical_coluna = 5;
 
-    // Fazendo a validação das posições dos navios
-    int h = horizontal_coluna < 8 ? 1 : 0;
-    int v = vertical_linha < 8 ? 1 : 0;
+    // Posição do navio diagonal principal
+    int principal_linha = 6;
+    int principal_coluna = 1;
 
-    if (h == 1 && v == 1)
+    // Posição do navio diagonal secundária
+    int secundaria_linha = 1;
+    int secundaria_coluna = 8;
+
+    // Posição do cone
+    int cone_linha = 7;
+    int cone_coluna = 7;
+
+    // Posição da cruz
+    int cruz_linha = 2;
+    int cruz_coluna = 2;
+
+    // Posição losango
+    int losango_linha = 6;
+    int losango_coluna = 4;
+
+    // Fazendo a validação das posições dos navios
+    int h = horizontal_coluna + tam_navio <= 10 ? 1 : 0;
+    int v = vertical_linha + tam_navio <= 10 ? 1 : 0;
+    int p = principal_linha + tam_navio <= 10 && principal_coluna + tam_navio <= 10 ? 1 : 0;
+    int s = secundaria_linha +tam_navio <= 10 && secundaria_coluna - tam_navio >= 0 ? 1 : 0;
+    int co = cone_linha < 8 && cone_coluna > 1 && cone_coluna < 8 ? 1 : 0;
+    int cr = cruz_linha > 1 && cruz_linha < 8 && cruz_coluna > 1 && cruz_coluna < 8 ? 1 : 0;
+    int l = losango_linha > 1 && losango_linha < 8 && losango_coluna > 1 && losango_coluna < 8 ? 1 : 0;
+
+
+    if (h == 1 && v == 1 && p == 1 && s == 1 && co == 1 && cr == 1 && l ==1)
     {
-        navio_horizontal(tabuleiro, 3, horizontal_linha, horizontal_coluna);
-        navio_vertical(tabuleiro, 3, vertical_linha, vertical_coluna);
+        navio_horizontal(tabuleiro, tam_navio, horizontal_linha, horizontal_coluna);
+        navio_vertical(tabuleiro, tam_navio, vertical_linha, vertical_coluna);
+        navio_diagonal_principal(tabuleiro, tam_navio, principal_linha, principal_coluna);
+        navio_diagonal_secundaria(tabuleiro, tam_navio, secundaria_linha, secundaria_coluna);
+        cone(tabuleiro, cone_linha, cone_coluna);
+        cruz(tabuleiro, cruz_linha, cruz_coluna);
+        losango(tabuleiro, losango_linha, losango_coluna);
         print_tabuleiro(tabuleiro);
     }
     else
